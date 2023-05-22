4d2ae6494c184718ab7b8f429b2b4c9c86489095
Sorini92 <artembedlinsky@gmail.com>
2023-05-22 12:20:55 +0300

    add some functions

--- src/components/WordsQuiz/WordsQuiz.js
+++ src/components/WordsQuiz/WordsQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect, useState, Fragment } from 'react';
 import './wordsQuiz.scss';
 import { useSelector } from 'react-redux';
+import Spinner from '../Spinner/Spinner';
 
 const WordsQuiz = ({setVariant, setActive}) => {
 
@@ -13,28 +14,42 @@ const WordsQuiz = ({setVariant, setActive}) => {
     const [index, setIndex] = useState('');
 
     useEffect(() => {
+        showQuiz();        
+        // eslint-disable-next-line
+    }, [words.length])
+
+    const showQuiz = () => {
         let copiedArray = [...words];
         const randomIndex = getRandomInt(copiedArray.length);
+        const correctAnswer = copiedArray[randomIndex].russian;
         const variantsOfAnswers = [];
         
         for (let i = 0; i < 3; i++) {
             if (randomIndex !== getRandomInt(copiedArray.length)) {
                 variantsOfAnswers.push(copiedArray[getRandomInt(copiedArray.length)].russian);
-                console.log(i)
-            }           
+            } else {
+                i = i - 1;
+            }          
         }
-        console.log(copiedArray[randomIndex].russian)
-        variantsOfAnswers.push(correct)
-        setVariants(variantsOfAnswers)
-        setCorrect(copiedArray[randomIndex].russian)
-        setOneQuestion([copiedArray[randomIndex].english])
-        // eslint-disable-next-line
-    }, [words.length])
+
+        variantsOfAnswers.push(correctAnswer);
+        
+        setVariants(variantsOfAnswers);
+        setCorrect(correctAnswer);
+        setOneQuestion([copiedArray[randomIndex].english]);
+        setAnswer('');
+        setIndex('');
+    }
 
     const getRandomInt = (max) => {
         return Math.floor(Math.random() * Math.floor(max));
     };
-    
+
+    const handleClick = (answer, i) => {
+        setIndex(i)
+        setAnswer(answer)
+    }
+
     const elements = oneQuestion.map((question, i) => {
         return (
             <Fragment key={i}>
@@ -42,7 +57,13 @@ const WordsQuiz = ({setVariant, setActive}) => {
                 <ul className='wordsquiz__variants'>
                     {variants.map((item, i) => {
                         return (
-                            <li key={i} className='wordsquiz__variant'>{item}</li>
+                            <li 
+                                onClick={() => handleClick(item, i)} 
+                                key={i} 
+                                className={index === i ? 'wordsquiz__variant activeAnswer' : 'wordsquiz__variant'}
+                            >
+                                {item}
+                            </li>
                         )
                     })}
                 </ul>
@@ -56,12 +77,12 @@ const WordsQuiz = ({setVariant, setActive}) => {
                 <div onClick={() => setActive(false)} className='wordsquiz__close'>&times;</div>
 
                 <div className='wordsquiz__wrapper'>
-                    {elements}
+                    {wordsLoadingStatus === 'loading' ? <Spinner/> : elements}
                 </div>
 
                 <div className='wordsquiz__btns'>
                     <button className='wordsquiz__btn' onClick={() => setVariant('')}>To main page</button>
-                    <button className='wordsquiz__btn'>Next quistion</button>
+                    <button className='wordsquiz__btn' onClick={() => showQuiz()}>Next quistion</button>
                 </div>
             </div>
         </div>
